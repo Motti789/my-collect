6ffcaca756c7500474aaae584e4ce9a4cd1a4060
Motti789 <mchillywilly@gmail.com>
2021-07-02 14:50:30 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+require "pry"
+def my_collect(collection)
+    collection = []
+    binding.pry
+    i = 0
+    while i < collection.length
+      collection <<  yield(collection[1])
+        i += 1
+    end
+end
 
 
